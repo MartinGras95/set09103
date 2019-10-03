afd6ba0453c3f03038adb566eed877d10f8e1615
Martin Gras <martin.gras95@gmail.com>
2019-10-03 10:48:04 +0100

    conditional templating

--- ninja.py
+++ ninja.py
@@ -11,5 +11,22 @@ def hello(name=None):
     user = {'name':name}
     return render_template('hello.html',user=user)
 
+
+#Conditional templating
+@app.route('/condhello/')
+@app.route('/condhello/<name>')
+def condhello(name=None):
+    return render_template('conditional.html', name=name)
+
+
+
+
+
+#404 handler
+@app.errorhandler(404)
+def page_not_found(error):
+    return "A ninja stole this page D:"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
+
